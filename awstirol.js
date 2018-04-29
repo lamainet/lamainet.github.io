6889663bb9befcf04333b1fe887b497d99665a08
Laura Mainetti <laura.mainetti@gmx.at>
2018-04-29 13:20:43 +0200

    Basemap und Orthofoto als Hintergrundkarte hinzugefügt

---
+++ awstirol.js
@@ -0,0 +1,21 @@
+let myMap = L.map("mapdiv");
+let markerGroup = L.featureGroup();
+myMap.addLayer(markerGroup);
+let myLayers = {
+    geolandbasemap : L.tileLayer(
+        "https://{s}.wien.gv.at/basemap/geolandbasemap/normal/google3857/{z}/{y}/{x}.png", {
+            subdomains : ["maps","maps1","maps2","maps3","maps4"],
+            attribution : "Datenquelle: <a href='https://www.basemap.at'>basemap.at</a>"
+    }
+    ),
+    bmaporthofoto30cm : L.tileLayer(
+        "https://{s}.wien.gv.at/basemap/bmaporthofoto30cm/normal/google3857/{z}/{y}/{x}.jpeg", {
+            subdomains : ["maps","maps1","maps2","maps3","maps4"],
+            attribution : "Datenquelle: <a href='https://www.basemap.at'>basemap.at</a>"
+    }
+    ),
+};
+
+myMap.addLayer(myLayers.geolandbasemap);
+
+myMap.setView([47.267,11.383], 11);
