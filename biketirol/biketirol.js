f62df8155e9183a9fc9178bf26088e3cbd30dd0b
Laura Mainetti <laura.mainetti@gmx.at>
2018-05-21 09:21:58 +0200

    biketirol.js korrigiert und aufgeräumt

--- biketirol/biketirol.js
+++ biketirol/biketirol.js
@@ -11,28 +11,28 @@
     -> statt 00 natürlich die eigene Etappe (z.B. 01,02, ...25)
 */	//CHECK
 
-// eine neue Leaflet Karte definieren	CHECK
+// eine neue Leaflet Karte definieren
 
 // Grundkartenlayer mit OSM, basemap.at, Elektronische Karte Tirol (Sommer, Winter, Orthophoto jeweils mit Beschriftung) über L.featureGroup([]) definieren
-// WMTS URLs siehe https://www.data.gv.at/katalog/dataset/land-tirol_elektronischekartetirol	THEORETISCH: CHECK
+// WMTS URLs siehe https://www.data.gv.at/katalog/dataset/land-tirol_elektronischekartetirol
 
-// Maßstab metrisch ohne inch	CHECK
+// Maßstab metrisch ohne inch
 
-// Start- und Endpunkte der Route als Marker mit Popup, Namen, Wikipedia Link und passenden Icons für Start/Ziel von https://mapicons.mapsmarker.com/	THEORETISCH CHECK
+// Start- und Endpunkte der Route als Marker mit Popup, Namen, Wikipedia Link und passenden Icons für Start/Ziel von https://mapicons.mapsmarker.com/
 
-// GeoJSON Track als Linie in der Karte einzeichnen und auf Ausschnitt zoomen	THEORETISCH CHECK
-// Einbauen nicht über async, sondern über ein L.geoJSON() mit einem Javascript Objekt (wie beim ersten Stadtspaziergang Wien Beispiel)	THEORETISCH CHECK
+// GeoJSON Track als Linie in der Karte einzeichnen und auf Ausschnitt zoomen
+// Einbauen nicht über async, sondern über ein L.geoJSON() mit einem Javascript Objekt (wie beim ersten Stadtspaziergang Wien Beispiel)
 
-// Baselayer control für OSM, basemap.at, Elektronische Karte Tirol hinzufügen	THEORETISCH CHECK
+// Baselayer control für OSM, basemap.at, Elektronische Karte Tirol hinzufügen
 
-// Overlay controls zum unabhängigem Ein-/Ausschalten der Route und Marker hinzufügen	THEORETISCH CHECK
+// Overlay controls zum unabhängigem Ein-/Ausschalten der Route und Marker hinzufügen
 
 
 
 
 let myMap = L.map("map");
 const trackGroup = L.featureGroup();
-//const markerGroup = L.featureGoup();
+const markerGroup = L.featureGroup();
 
 
 let myLayers = {
@@ -54,28 +54,26 @@ let myLayers = {
         attribution: "Datenquelle: <a href='https://www.basemap.at'>basemap.at</a>"
         }
     ),
-    /*
     tirolsommer : L.tileLayer(
     	"http://wmts.kartetirol.at/wmts/gdi_base_summer/GoogleMapsCompatible/{z}/{x}/{y}.jpeg80", {
-    	attribution: "Datenquelle: <a href='https://www.data.gv.at/katalog/dataset/land-tirol_elektronischekartetirol'>data.gv.at</a>
+    	attribution: "Datenquelle: <a href='https://www.data.gv.at/katalog/dataset/land-tirol_elektronischekartetirol'>data.gv.at</a>"
     }
     ),
     tirolwinter : L.tileLayer(
     	"http://wmts.kartetirol.at/wmts/gdi_base_winter/GoogleMapsCompatible/{z}/{x}/{y}.jpeg80", {
-    	attribution: "Datenquelle: <a href='https://www.data.gv.at/katalog/dataset/land-tirol_elektronischekartetirol'>data.gv.at</a>
+    	attribution: "Datenquelle: <a href='https://www.data.gv.at/katalog/dataset/land-tirol_elektronischekartetirol'>data.gv.at</a>"
     }
     ),
     tirolortho : L.tileLayer(
     	"http://wmts.kartetirol.at/wmts/gdi_ortho/GoogleMapsCompatible/{z}/{x}/{y}.jpeg80", {
-    	attribution: "Datenquelle: <a href='https://www.data.gv.at/katalog/dataset/land-tirol_elektronischekartetirol'>data.gv.at</a>
+    	attribution: "Datenquelle: <a href='https://www.data.gv.at/katalog/dataset/land-tirol_elektronischekartetirol'>data.gv.at</a>"
     }
     ),
     tirolname : L.tileLayer(
     	"http://wmts.kartetirol.at/wmts/gdi_nomenklatur/GoogleMapsCompatible/{z}/{x}/{y}.png8", {
-    	attribution: "Datenquelle: <a href='https://www.data.gv.at/katalog/dataset/land-tirol_elektronischekartetirol'>data.gv.at</a>
+    	attribution: "Datenquelle: <a href='https://www.data.gv.at/katalog/dataset/land-tirol_elektronischekartetirol'>data.gv.at</a>"
     }
     ),
-    */
 };
 
 
@@ -87,14 +85,12 @@ myMap.addLayer(myLayers.geolandbasemap);
 let myMapControl = L.control.layers({
     "Openstreetmap" : myLayers.osm,
     "Basemap" : myLayers.geolandbasemap,
-   	/*
     "Sommer" : myLayers.tirolsommer,
     "Winter" : myLayers.tirolwinter,
     "Orthofoto" : myLayers.tirolortho,
-    */
 }, {
     "Overlay" : myLayers.bmapoverlay,
-    //"Start-Ziel" : markerGroup,
+    "Start-Ziel" : markerGroup,
    	"Route" : trackGroup,
 }, {
 	position : "topright"
@@ -141,83 +137,46 @@ gpxTrack.on("loaded", function(evt) {
 
 
 
+/* GeoJson einbinden über Objekt in einer .js Datei
 
-
-/*
 let geojson = L.geoJSON(traildata).addTo(trailGroup);
 
 myMap.addLayer(trailGroup);
 
 myMap.fitBounds(trailGroup.getBounds());
-
-/*
-let line = L.polyline(geojson, {color: 'blue'}).addTo(myMap);
 */
 
 
 
-
-/*
 let startIcon = L.icon({
-	iconUrl: 'start.png'
+	iconUrl: './images/start.png'
 })
 
 
 let endIcon = L.icon({
-	iconUrl: 'end.png'
+	iconUrl: './images/end.png'
 })
 
 const start = [47.298546,11.665867];
 const end = [47.261733,11.553046];
 
-L.marker(start, {
+let startMarker = L.marker(start, {
 	icon: startIcon
 }).addTo(markerGroup)
 
-
-L.marker(end, {
+let endMarker = L.marker(end, {
 	icon: endIcon
 }).addTo(markerGroup)
 
 
 
-start.bindPopup("<p>Start<br /><a href='https://de.wikipedia.org/wiki/Weerberg'>Weerberg</a>").openPopup();
-
-end.bindPopup("<p>Ende<br /><a href='https://de.wikipedia.org/w/index.php?title=Windegg_(Gemeinde_Tulfes)&action=edit&redlink=1'>Windegg</a>").openPopup();
-
+startMarker.bindPopup("<p>Start<br /><a href='https://de.wikipedia.org/wiki/Weerberg'>Weerberg</a>").openPopup();
+endMarker.bindPopup("<p>Ende<br /><a href='https://de.wikipedia.org/w/index.php?title=Windegg_(Gemeinde_Tulfes)&action=edit&redlink=1'>Windegg</a>").openPopup();
 
 myMap.addLayer(markerGroup);
-
-
 myMap.fitBounds(markerGroup.getBounds());
 
 
 
 
 
-/*
-VERSUCH 2
-
-let latlong = coordsToLatLng(traildata);
-
-latlong.addTo(trailGroup);
-
-myMap.addLayer(trailGroup);
-
-myMap.fitBounds(trailGroup.getBounds());
-*/
-
-
-
-
-/*
-VERSUCH 3
-
-let geojson = L.geoJSON(traildata);
-
-geojson.addTo(trailGroup);
-
-myMap.addLayer(trailGroup);
-
-myMap.fitBounds(trailGroup.getBounds());
-*/
